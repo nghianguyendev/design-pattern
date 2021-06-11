be171488248abde93bd8644a3c99861df853a8de
Nghia Nguyen <nghianguyen@bbv.ch>
2021-06-11 15:27:00 +0700

    Composite design pattern

---
+++ Composite/Composite.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Composite
+{
+    public interface IEmployee
+    {
+        string Name { get; set; }
+    }
+
+    public class CoreEmployee : IEmployee, IEnumerable<IEmployee>
+    {
+        private List<IEmployee> _subOrdinates = new List<IEmployee>();
+        public string Name { get; set; }
+
+        public void Add(IEmployee employee)
+        {
+            _subOrdinates.Add(employee);
+        }
+
+        public IEnumerator<IEmployee> GetEnumerator()
+        {
+            return _subOrdinates.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _subOrdinates.GetEnumerator();
+        }
+    }
+
+    public class ContractEmployee : IEmployee
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Composite Design pattern!");
+
+            var manager = new CoreEmployee { Name = "Manager" };
+            var teamleader = new CoreEmployee { Name = "Team Lead" };
+            var member1 = new CoreEmployee { Name = "Member1" };
+            var member2 = new CoreEmployee { Name = "Member2" };
+            var contract = new ContractEmployee { Name = "Contract" };
+
+            teamleader.Add(member1);
+            teamleader.Add(member2);
+            member2.Add(contract);
+
+            manager.Add(teamleader);
+
+            EmployeeExtension.PrintHierarchy(manager);
+        }
+    }
+
+    public static class EmployeeExtension
+    {
+        public static void PrintHierarchy(this CoreEmployee employee)
+        {
+            foreach (CoreEmployee item in employee)
+            {
+                Console.WriteLine($"{item.Name}");
+                foreach (CoreEmployee item1 in item)
+                {
+                    Console.WriteLine($"  {item1.Name}");
+                    foreach (ContractEmployee item2 in item1)
+                    {
+                        Console.WriteLine($"    {item2.Name}");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
