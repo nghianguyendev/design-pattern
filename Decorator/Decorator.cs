eeb93f59d560a060258c61c1c1b864bece1dd4ca
Nghia Nguyen <nghianguyen@bbv.ch>
2021-06-11 17:13:38 +0700

    Decorator design pattern

---
+++ Decorator/Decorator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class LegacyOldCar
+    {
+        public string GetName()
+        {
+            return "Legacy Old Car";
+        }
+
+        public double GetCost()
+        {
+            return 1000;
+        }
+    }
+
+    // Adding new functionality to LegacyOldCar
+
+    public interface ICar
+    {
+        string GetName();
+        double GetCost();
+    }
+    public class LegacyOldCarDecorator : ICar
+    {
+        private LegacyOldCar _legacyOldCar;
+
+        public LegacyOldCarDecorator(LegacyOldCar legacyOldCar)
+        {
+            this._legacyOldCar = legacyOldCar;
+        }
+
+        public virtual double GetCost()
+        {
+            return _legacyOldCar.GetCost();
+        }
+
+        public virtual string GetName()
+        {
+            return _legacyOldCar.GetName();
+        }
+    }
+
+    public class LegacyOldCarWithNewIndicator : LegacyOldCarDecorator
+    {
+        public LegacyOldCarWithNewIndicator(LegacyOldCar legacyOldCar) : base(legacyOldCar)
+        {
+        }
+
+        public override double GetCost()
+        {
+            return base.GetCost() + 100;
+        }
+
+        public override string GetName()
+        {
+            return base.GetName() + " with new indicator";
+        }
+
+    }
+
+    public class LegacyOldCarWithNewIndicatorAndWheel : LegacyOldCarWithNewIndicator
+    {
+        public LegacyOldCarWithNewIndicatorAndWheel(LegacyOldCar legacyOldCar) : base(legacyOldCar)
+        {
+        }
+
+        public override double GetCost()
+        {
+            return base.GetCost() + 200;
+        }
+
+        public override string GetName()
+        {
+            return base.GetName() + " with new wheel";
+        }
+
+    }
+
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Decorator design pattern!");
+
+            var legacyOldCar = new LegacyOldCar();
+            Console.WriteLine("Name: " + legacyOldCar.GetName());
+            Console.WriteLine("Cost: " + legacyOldCar.GetCost());
+
+            var legacyCarWithNewIndicator = new LegacyOldCarWithNewIndicator(legacyOldCar);
+            Console.WriteLine("Name: "+legacyCarWithNewIndicator.GetName());
+            Console.WriteLine("Cost: "+legacyCarWithNewIndicator.GetCost());
+
+            var legacyCarWithNewIndicatorAndWheel = new LegacyOldCarWithNewIndicatorAndWheel(legacyOldCar);
+            Console.WriteLine("Name: " + legacyCarWithNewIndicatorAndWheel.GetName());
+            Console.WriteLine("Cost: " + legacyCarWithNewIndicatorAndWheel.GetCost());
+        }
+    }
+}
