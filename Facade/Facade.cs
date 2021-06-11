a6cf17b5f72ba36c4dd0f8b2a497fb5d425a0139
Nghia Nguyen <nghianguyen@bbv.ch>
2021-06-11 18:30:41 +0700

    Facade design pattern

---
+++ Facade/Facade.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public interface IPizza
+    {
+        void GetVegPizza();
+        void GetNonVegPizza();
+    }
+
+    public class PizzaProvider : IPizza
+    {
+        public void GetNonVegPizza()
+        {
+            GetNonVegToppings();
+            Console.WriteLine("Getting Non Veg Pizza.");
+        }
+        public void GetVegPizza()
+        {
+            Console.WriteLine("Getting Veg Pizza.");
+        }
+        private void GetNonVegToppings()
+        {
+            Console.WriteLine("Getting Non Veg Pizza Toppings.");
+        }
+    }
+
+    public interface IBread
+    {
+        void GetGarlicBread();
+        void GetCheesyGarlicBread();
+    }
+
+    public class BreadProvider : IBread
+    {
+        public void GetGarlicBread()
+        {
+            Console.WriteLine("Getting Garlic Bread.");
+        }
+        public void GetCheesyGarlicBread()
+        {
+            GetCheese();
+            Console.WriteLine("Getting Cheesy Garlic Bread.");
+        }
+        private void GetCheese()
+        {
+            Console.WriteLine("Getting Cheese.");
+        }
+    }
+
+    public class RestaurantFacade
+    {
+        private IPizza _PizzaProvider;
+        private IBread _BreadProvider;
+        public RestaurantFacade()
+        {
+            _PizzaProvider = new PizzaProvider();
+            _BreadProvider = new BreadProvider();
+        }
+        public void GetNonVegPizza()
+        {
+            _PizzaProvider.GetNonVegPizza();
+        }
+        public void GetVegPizza()
+        {
+            _PizzaProvider.GetVegPizza();
+        }
+        public void GetGarlicBread()
+        {
+            _BreadProvider.GetGarlicBread();
+        }
+        public void GetCheesyGarlicBread()
+        {
+            _BreadProvider.GetCheesyGarlicBread();
+        }
+    }
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Facade
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Facade!");
+
+            Console.WriteLine("----------------------CLIENT ORDERS FOR PIZZA----------------------------\n");
+            var facadeForClient = new RestaurantFacade();
+            facadeForClient.GetNonVegPizza();
+            facadeForClient.GetVegPizza();
+            Console.WriteLine("\n----------------------CLIENT ORDERS FOR BREAD----------------------------\n");
+            facadeForClient.GetGarlicBread();
+            facadeForClient.GetCheesyGarlicBread();
+        }
+    }
+}
