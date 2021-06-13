e198bfcb2b263c681d65d9d9a6083b34b8ce7c5e
Nghia Nguyen <nghianguyen@bbv.ch>
2021-06-13 18:08:55 +0700

    Flyweight design pattern

---
+++ Flyweight/Flyweight.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Flyweight
+{
+    public class FlyweightFactory
+    {
+        public static IMemoryCache cache;
+
+        public ICountryManager ICountryManager
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public static IEnumerable<Country> GetCountries()
+        {
+            ICountryManager countryManager = new CountryManager();
+
+            var countries = cache.Get<IEnumerable<Country>>("country");
+            if (countries != null)
+            {
+                return countries;
+            }
+
+            countries = countryManager.GetCountries();
+            cache.Set("country", countries);
+            return countries;
+        }
+    }
+
+    public interface ICountryManager
+    {
+        IEnumerable<Country> GetCountries();
+    }
+
+    public class CountryManager : ICountryManager
+    {
+        public Country Country
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public IEnumerable<Country> GetCountries()
+        {
+            var countries = new List<Country>
+            {
+                new Country{ Id=Guid.NewGuid(),Name="VietNam" },
+                new Country{ Id=Guid.NewGuid(),Name="Australia" },
+                new Country{ Id=Guid.NewGuid(),Name="America" }
+            };
+            return countries.AsEnumerable();
+        }
+    }
+
+    public class Country
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Flyweight/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+
+
+namespace Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Flyweight design pattern!");
+
+            var serviceCollection = new ServiceCollection().AddMemoryCache().BuildServiceProvider();
+
+            FlyweightFactory.cache = serviceCollection.GetService<IMemoryCache>();
+
+            var countries = FlyweightFactory.GetCountries();
+
+            // will be returned from Cache
+            var countriesCalledFromOtherPlace = FlyweightFactory.GetCountries();
+
+            Console.WriteLine("These countries will be returned from cache!");
+            foreach (var item in countries)
+            {
+                Console.WriteLine($"Id: {item.Id} Name: {item.Name}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
