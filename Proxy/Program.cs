302023a2c7a09d176dfa34c2b09b28309d38ba53
Nghia Nguyen <nghianguyen@bbv.ch>
2021-06-14 23:47:10 +0700

    Proxy design pattern

---
+++ Proxy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Proxy design pattern!");
+
+            var proxyInternetAccess = new ProxyInternetAccess();
+
+            proxyInternetAccess.ConnectTo("mysite.net");
+            
+            //will throw exception
+            proxyInternetAccess.ConnectTo("google.com");
+        }
+    }
+}
---
+++ Proxy/Proxy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Proxy
+{
+    internal interface IInternetAccess
+    {
+        void ConnectTo(string address);
+    }
+
+    internal class RealInternetAccess : IInternetAccess
+    {
+        public void ConnectTo(string address)
+        {
+            Console.WriteLine("Connecting to " + address);
+        }
+    }
+
+    internal class ProxyInternetAccess : IInternetAccess
+    {
+        private List<string> bannedSites = new List<string> {
+            "google.com",
+            "microsoft.com"
+        };
+
+        private RealInternetAccess internetAccess = new RealInternetAccess();
+
+        public void ConnectTo(string address)
+        {
+            if (bannedSites.Contains(address))
+            {
+                Console.WriteLine($"{address} Access denied");
+                return;
+            }
+
+            internetAccess.ConnectTo(address);
+        }
+    }
+}
