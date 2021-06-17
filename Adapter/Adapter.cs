eaaa39caf03653fabba74c8262f4b822e0aa1bc6
Nghia Nguyen <nghianguyen@bbv.ch>
2021-06-17 07:20:14 +0700

    Adapter design pattern

---
+++ Adapter/Adapter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Adapter
+{
+    internal class MessageResource
+    {
+        public IEnumerable<string> GetMessages()
+        {
+            return new List<string> {
+                "One message",
+                "Another message",
+                "Third message"
+            };
+        }
+    }
+
+    internal interface IMessageProvider
+    {
+        IEnumerable<string> GetMessages();
+    }
+
+    internal class MessageProvider : IMessageProvider
+    {
+        private MessageResource _messageResource = new MessageResource();
+
+        public IEnumerable<string> GetMessages()
+        {
+            return ModifyMessages(_messageResource.GetMessages());
+        }
+
+        private IEnumerable<string> ModifyMessages(IEnumerable<string> messages)
+        {
+            return messages.ToList().Select(x => $"From me: {x}").AsEnumerable();
+        }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Adapter Design Pattern!");
+
+            var messageProvider = new MessageProvider();
+            var messages = messageProvider.GetMessages();
+            Console.WriteLine(messages.FirstOrDefault());
+        }
+    }
+}
