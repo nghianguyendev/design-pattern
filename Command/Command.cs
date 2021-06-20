42f10bcd8e555ab31abc1cc0002da054bf75d7a1
Nghia Nguyen <nghianguyen@bbv.ch>
2021-06-21 00:32:42 +0700

    Command design pattern

---
+++ Command/Command.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Command
+{
+    internal class AirConditioner
+    {
+        public void On()
+        {
+            Console.WriteLine("On");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("Off");
+        }
+    }
+
+    internal abstract class Command
+    {
+        protected AirConditioner _airConditioner;
+
+        public Command(AirConditioner airConditioner)
+        {
+            this._airConditioner = airConditioner;
+        }
+
+        public abstract void Execute();
+    }
+
+    internal class OnCommand : Command
+    {
+        public OnCommand(AirConditioner airConditioner) : base(airConditioner)
+        {
+        }
+
+        public override void Execute()
+        {
+            _airConditioner.On();
+        }
+    }
+
+    internal class OffCommand : Command
+    {
+        public OffCommand(AirConditioner airConditioner) : base(airConditioner)
+        {
+        }
+
+        public override void Execute()
+        {
+            _airConditioner.Off();
+        }
+    }
+
+    internal class RemoteInvoker
+    {
+        private Command _command;
+
+        public RemoteInvoker(Command command)
+        {
+            _command = command;
+        }
+
+        public void SetCommand(Command command)
+        {
+            _command = command;
+        }
+
+        public void PressButton()
+        {
+            _command.Execute();
+        }
+    }
+}
---
+++ Command/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Command Design Pattern!");
+
+            var airConditioner = new AirConditioner();
+            var oncommand = new OnCommand(airConditioner);
+            var remoteInvoker = new RemoteInvoker(oncommand);
+
+            remoteInvoker.PressButton();
+
+            var offcommand = new OffCommand(airConditioner);
+            remoteInvoker.SetCommand(offcommand);
+            remoteInvoker.PressButton();
+        }
+    }
+}
