8dbcc43137930a683637e1e5e50ea216e75a616a
Nghia Nguyen <nghianguyen@bbv.ch>
2021-06-20 17:58:33 +0700

    Chain Of responsibility design pattern

---
+++ ChainOfResponsibility/ChainOfResponsibility.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    internal abstract class BaseApprover
+    {
+        private BaseApprover next;
+
+        public void Approve(int days)
+        {
+            if (CanApprove(days))
+            {
+                Console.WriteLine($"Your submit leave has been approved by {this.GetType().Name}!");
+            }
+            else
+            {
+                if (next != null)
+                {
+                    next.Approve(days);
+                }
+                else
+                {
+                    Console.WriteLine("Your submit leave can not be approved!");
+                }
+            }
+        }
+
+        protected abstract bool CanApprove(int days);
+
+        public void SetNextApprover(BaseApprover approver)
+        {
+            next = approver;
+        }
+    }
+
+    internal class TeamLeadApprover : BaseApprover
+    {
+        protected override bool CanApprove(int days)
+        {
+            return days <= 3;
+        }
+    }
+
+    internal class ProjectManagerApprover : BaseApprover
+    {
+        protected override bool CanApprove(int days)
+        {
+            return days <= 7;
+        }
+    }
+
+    internal class DeliveryManagerApprover : BaseApprover
+    {
+        protected override bool CanApprove(int days)
+        {
+            return days <= 10;
+        }
+    }
+
+    internal class CEOManagerApprover : BaseApprover
+    {
+        protected override bool CanApprove(int days)
+        {
+            return days <= 15;
+        }
+    }
+}
---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Chain of Responsibility pattern!");
+
+            var tlApprover = new TeamLeadApprover();
+            var pmApprover = new ProjectManagerApprover();
+            var dmApprover = new DeliveryManagerApprover();
+            var ceoApprover = new CEOManagerApprover();
+
+            tlApprover.SetNextApprover(pmApprover);
+            pmApprover.SetNextApprover(dmApprover);
+            dmApprover.SetNextApprover(ceoApprover);
+
+            tlApprover.Approve(3);
+            tlApprover.Approve(7);
+            tlApprover.Approve(10);
+            tlApprover.Approve(15);
+
+            tlApprover.Approve(30);
+
+        }
+    }
+}
