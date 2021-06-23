074fae4b0a3e239160be64c6abe5b20c7fde34aa
Nghia Nguyen <nghianguyen@bbv.ch>
2021-06-23 23:57:41 +0700

    Interpreter design pattern

---
+++ Interpreter/Interpreter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    public class Context
+    {
+        public DateTime date { get; set; }
+        public string expression { get; set; }
+    }
+
+    public interface IDateExpression
+    {
+        void Evaluate(Context context);
+    }
+
+    public class YearExpression : IDateExpression
+    {
+        public void Evaluate(Context context)
+        {
+            context.expression = context.expression.Replace("YYYY", context.date.Year.ToString());
+        }
+    }
+
+    public class MonthExpression : IDateExpression
+    {
+        public void Evaluate(Context context)
+        {
+            context.expression = context.expression.Replace("MM", context.date.Month.ToString());
+        }
+    }
+
+    public class DayExpression : IDateExpression
+    {
+        public void Evaluate(Context context)
+        {
+            context.expression = context.expression.Replace("DD", context.date.Day.ToString());
+        }
+    }
+
+}
---
+++ Interpreter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Interpreter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Interpreter design pattern!");
+
+            var context = new Context
+            {
+                date = DateTime.Now,
+                expression = "DD/MM/YYYY"
+            };
+
+            var yearExpression = new YearExpression();
+            var monthExpression = new MonthExpression();
+            var dayExpression = new DayExpression();
+
+            yearExpression.Evaluate(context);
+            monthExpression.Evaluate(context);
+            dayExpression.Evaluate(context);
+
+            Console.WriteLine(context.expression);
+        }
+    }
+}
