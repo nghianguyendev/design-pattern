9044196496e089a02974a9ad9a78a08005c7ea06
Nghia Nguyen <nghianguyen@bbv.ch>
2021-07-03 00:37:22 +0700

    Memento

---
+++ Memento/Memento.cs
@@ -0,0 +1,43 @@
+﻿namespace Memento
+{
+    internal class Memento
+    {
+        public double Balance { get; set; }
+
+        public Memento(double balance)
+        {
+            this.Balance = balance;
+        }
+    }
+
+    internal class CareTaker
+    {
+        public Memento CheckPoint { get; set; }
+    }
+
+    internal class BankAccount
+    {
+        private double balance;
+
+        public BankAccount(double balance)
+        {
+            this.balance = balance;
+        }
+
+        public Memento Deposit(double amount)
+        {
+            this.balance += amount;
+            return new Memento(balance);
+        }
+
+        public void Restore(Memento memento)
+        {
+            this.balance = memento.Balance;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(balance)} : {balance}";
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Memento Design Pattern!");
+
+            var bankAccount = new BankAccount(100);
+            Console.WriteLine(bankAccount.ToString());
+
+            var careTaker = new CareTaker();
+            careTaker.CheckPoint = bankAccount.Deposit(100);
+            Console.WriteLine($"First deposit: {bankAccount.ToString()}");
+
+            var savePoint2 = bankAccount.Deposit(100);
+            Console.WriteLine($"Second deposit: {bankAccount.ToString()}");
+
+            Console.WriteLine("Restore to First deposit");
+            bankAccount.Restore(careTaker.CheckPoint);
+            Console.WriteLine(bankAccount.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
