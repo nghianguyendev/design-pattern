b2a85644ff38c22484bc13da39531714ce35a234
Nghia Nguyen <nghianguyen@bbv.ch>
2021-07-04 17:48:41 +0700

    Null object

---
+++ NullObject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NullObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Null Object Design Pattern!");
+
+            var log = Null<ILog>.Instance;
+            //var log = new ConsoleLog();
+            var ba = new BankAccount(log);
+            ba.Deposit(100);
+            ba.Withdraw(200);
+        }
+    }
+}
