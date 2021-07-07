c1ca623cf45bd90ab85427cbfd44b2499aff15ac
Nghia Nguyen <nghianguyen@bbv.ch>
2021-07-07 13:03:24 +0700

    Strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Strategy design pattern!");
+
+            var logService = new LogService();
+            logService.Log(LogType.Console, "Console message");
+            logService.Log(LogType.File, "File message");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Strategy/Strategy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strategy
+{
+    internal enum LogType
+    {
+        Console,
+        File
+    }
+
+    internal interface ILog
+    {
+        void Info(string message);
+    }
+
+    internal class ConsoleLog : ILog
+    {
+        public void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    internal class FileLog : ILog
+    {
+        public void Info(string message)
+        {
+            Console.WriteLine($"Write to file the message: {message}");
+        }
+    }
+
+    internal class LogService
+    {
+        private IDictionary<LogType, ILog> _log = new Dictionary<LogType, ILog>();
+
+        public LogService()
+        {
+            _log.Add(LogType.Console, new ConsoleLog());
+            _log.Add(LogType.File, new FileLog());
+        }
+
+        public void Log(LogType logType, string message)
+        {
+            _log[logType].Info(message);
+        }
+    }
+}
