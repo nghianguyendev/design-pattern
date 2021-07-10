0b8702a404206c1375c6f58da045ef1c57517754
Nghia Nguyen <nghianguyen@bbv.ch>
2021-07-11 00:56:54 +0700

    Template Method

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Template Method design pattern!");
+            var chess = new Chess();
+            chess.Run();
+        }
+    }
+}
