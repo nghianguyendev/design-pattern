8f5f4d0c1a408ab4b23f2b16c5e5b0c56038752a
Nghia Nguyen <nghianguyen@bbv.ch>
2021-07-11 14:09:21 +0700

    Visitor

---
+++ Visitor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Visitor design pattern!");
+
+            var swissEmployee = new SwissEmployee("Nghia", 1000);
+            var export = new ExportVisitor();
+            export.Export(swissEmployee);
+
+            var egyptEmployee = new EgyptEmployee("Sopanos", 1000);
+            egyptEmployee.Accept(export);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Visitor/Visitor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Visitor
+{
+    internal abstract class Employee
+    {
+        public string Name { get; set; }
+
+        public int Salary { get; set; }
+
+        public Employee(string name, int salary)
+        {
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        /// <summary>
+        /// Double dispatch: a strict that allows dynamic binding alongside with overload methods
+        /// 
+        /// The `accept` method is overriden, not overloaded. Compiler binds it
+        /// dynamically. Therefore the `accept` will be executed on a class that
+        /// corresponds to an object calling a method (in our case, the `Dot` class).
+        /// 
+        /// </summary>
+        /// <param name="visitor"></param>
+        public abstract void Accept(IExportVisitor visitor);
+    }
+
+    internal class VietnamEmployee : Employee
+    {
+        public VietnamEmployee(string name, int salary) : base(name, salary)
+        {
+        }
+
+        public override void Accept(IExportVisitor visitor)
+        {
+            visitor.Export(this);
+        }
+    }
+
+    internal class SwissEmployee : Employee
+    {
+        public SwissEmployee(string name, int salary) : base(name, salary)
+        {
+        }
+        public override void Accept(IExportVisitor visitor)
+        {
+            visitor.Export(this);
+        }
+    }
+
+    internal class EgyptEmployee : Employee
+    {
+        public EgyptEmployee(string name, int salary) : base(name, salary)
+        {
+        }
+
+        public override void Accept(IExportVisitor visitor)
+        {
+            visitor.Export(this);
+        }
+    }
+
+    internal interface IExportVisitor
+    {
+        void Export(VietnamEmployee vietnamEmployee);
+        void Export(SwissEmployee swissEmployee);
+        void Export(EgyptEmployee egyptEmployee);
+    }
+
+    class ExportVisitor : IExportVisitor
+    {
+        public void Export(VietnamEmployee employee)
+        {
+            Console.WriteLine($"Vietnam salary of {employee.Name} is {employee.Salary}");
+        }
+
+        public void Export(SwissEmployee employee)
+        {
+            Console.WriteLine($"Swiss salary of {employee.Name} is {employee.Salary}");
+        }
+
+        public void Export(EgyptEmployee employee)
+        {
+            Console.WriteLine($"Egypt salary of {employee.Name} is {employee.Salary}");
+        }
+    }
+}
